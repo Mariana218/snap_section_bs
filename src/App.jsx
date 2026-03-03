388f2bfd03247093247f4a98243eaeb2a0b2d307
Mariana218 <mcmariana218@gmail.com>
2026-03-03 11:38:23 -0500

    version5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Header } from './components/Header'
 import { Careers } from './pages/Careers'
 import { Home } from './pages/Home'
+import { About } from './pages/About'
 
 export const App = () => {
   return (
     <>
     
     {/* <Header/>
-    <Home/> */}
     <Careers/>
+    <Home/> */}
+
+    <About/>
 
 
     </>
--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -40,19 +40,42 @@ export const Careers = () => {
     <Tarjets image='./public/icon-whole-food-recipes.svg' name='Search in seconds' description='Each dish uses everyday, unprocessed ingredients.'/>
     </div>
     <section className="container">
-        
-            <div className="col-2">
-                <div className="d-flex flex-row">
-                <div className="">
-                    <h1>Built for real life</h1>
-                    <p className="fs-5">Cooking shouldn’t be complicated. These recipes come in under 30 minutes of active time, fit busy schedules, and taste good enough to repeat.Whether you’re new to the kitchen or just need fresh ideas, we’ve got you covered.</p>
+        <div className="row">
+            <div className="col-2 w-100">
+            <div className="d-flex"> 
+                <div className="w-50">
+                    <div className="d-flex flex-column gap-3 m-5">
+                    <h1 className="fw-bold text-success">Built for real life</h1>
+                    <p className="fs-5 w-75">Cooking shouldn’t be complicated. These recipes come in under 30 minutes of active time, fit busy schedules, and taste good enough to repeat.Whether you’re new to the kitchen or just need fresh ideas, we’ve got you covered.</p>
+                    </div>
                 </div>
-                <div className="">
-                    <img className="w-100" src="./public/image-home-real-life-large.webp" alt="" />
+                <div className="w-50">
+                    <img className="w-100 p-3 border border-white border-5 rounded-5" src="./public/image-home-real-life-large.webp" alt="" />
                 </div>
                 </div>
             </div>
+        </div>
     </section>
+    <section className="container">
+        <div className="row">
+            <div className="col">
+                <div className="bg-info-subtle p-3 border border-white border-5 rounded-5 d-flex justify-content-center align-items-center">
+                    <div className="text-center p-5 m-3">
+                        <div>
+                        <h1 className="text-success fw-bold">Ready to cook smarter?</h1>
+                        <p className="fs-5">Hit the button, pick a recipe, and get dinner on the table—fast.</p>
+                        </div>
+
+                        <div className="m-5">
+                        <button type="button" class="btn btn-success fw-bold p-2 fs-5">Brose recipes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     </>
+
   )
 }
